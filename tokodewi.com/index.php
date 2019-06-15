9ec3d9128bd99747bd93d54acc0c79bfd2bdc452
yudha prasetya <seiya_matsushita@outlook.com>
2019-06-15 15:22:32 +0700

    ok all

---
+++ tokodewi.com/index.php
@@ -0,0 +1,24 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <style media="screen">
+      <?php include 'css/style.css'; ?>
+    </style>
+    <meta charset="utf-8">
+    <title>Toko Dewi</title>
+  </head>
+  <body>
+
+    <h1 class="title">Toko Dewi</h1>
+
+    <div class="topnav">
+      <a href="#home">Home</a>
+      <a href="#daftar">Daftar Menu</a>
+      <a href="#harga">Harga Makanan</a>
+      <a href="#layan">Layanan</a>
+      <a href="#custom">Customer</a>
+    </div>
+  <label for=""></label>
+  </body>
+</html>
