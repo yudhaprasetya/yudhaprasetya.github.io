e6c34564b8f338f6332babce38acfb7fa189b28e
yudha prasetya <seiya_matsushita@outlook.com>
2019-06-17 13:03:51 +0700

    yes

---
+++ tokodewi.com/daftarmenu/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="id" dir="ltr">
+  <head>
+
+    <style media="screen">
+      <?php include '../css/style.css'; ?>
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Toko Dewi | Daftar Menu</title>
+  </head>
+  <body>
+    <h1></h1>
+
+    <?php include '../admin/topnav.php'; ?>
+
+    <h3>Daftar Makanan dan Minuman</h3>
+
+    <p><img style="float: left" src="" alt="Ayam Penyet">
+      Ayam Penyet <br> <br>
+      Price : <label for="">Rp 25.000</label>
+    </p>
+
+    <p style="text-align: right"><img style="float: right" src="" alt="Ayam Penyet">
+      Nasi Goreng <br><br>
+      Price : Rp 20.000
+    </p>
+
+    <p><img style="float: left" src="" alt="Ayam Penyet">
+      Ayam Penyet <br> <br>
+      Price : <label for="">Rp 25.000</label>
+    </p>
+
+    <p style="text-align: right"><img style="float: right" src="" alt="Ayam Penyet">
+      Nasi Goreng <br><br>
+      Price : Rp 20.000
+    </p>
+
+    <p><img style="float: left" src="" alt="Ayam Penyet">
+      Ayam Penyet <br> <br>
+      Price : <label for="">Rp 25.000</label>
+    </p>
+
+    <p style="text-align: right"><img style="float: right" src="" alt="Ayam Penyet">
+      Nasi Goreng <br><br>
+      Price : Rp 20.000
+    </p>
+
+    <p><img style="float: left" src="" alt="Ayam Penyet">
+      Ayam Penyet <br> <br>
+      Price : <label for="">Rp 25.000</label>
+    </p>
+
+    <p style="text-align: right"><img style="float: right" src="" alt="Ayam Penyet">
+      Nasi Goreng <br><br>
+      Price : Rp 20.000
+    </p>
+
+  </body>
+</html>
--- tokodewi.com/hargamakanan/index.php
+++ tokodewi.com/hargamakanan/index.php
@@ -9,7 +9,13 @@
   </head>
   <body>
 
-    <?php include 'admin/topnav.php'; ?>
+    <div class="topnav">
+      <a href="/">Home</a>
+      <a href="daftarmenu/">Daftar Menu</a>
+      <a href="hargamakanan/">Harga Makanan</a>
+      <a href="layanan/">Layanan</a>
+      <a href="customer/">Customer</a>
+    </div>
   <label for=""></label>
   </body>
 </html>
