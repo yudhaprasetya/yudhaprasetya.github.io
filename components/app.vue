8190ecd449f8c82075cb887c903a9f15b33f4af7
yudha prasetya <seiya_matsushita@outlook.com>
2019-07-24 11:59:59 +0700

    ok

---
+++ components/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container-fluid">
+    <h1>{{$root.title}}</h1>
+    <hr />
+    <div class="alert alert-success">It works!</div>
+  </div>
+</template>
+<style>
+  .container-fluid {
+    max-width: 500px;
+  }
+</style>
