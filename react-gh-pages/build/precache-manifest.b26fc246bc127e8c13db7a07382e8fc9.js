a61b7571319bdfaef4c71aa1464277bf71b9b674
Yudha Prasetya <yudhaprasetyausman@gmail.com>
2019-08-13 22:06:08 +0700

    ok

---
+++ react-gh-pages/build/precache-manifest.b26fc246bc127e8c13db7a07382e8fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6038bb67f5ac7d33a804a4ebba069b7",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9cfefc22859d870f0709",
+    "url": "/react-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "20f717d2548a28f90648",
+    "url": "/react-gh-pages/static/js/2.9e1cd209.chunk.js"
+  },
+  {
+    "revision": "9cfefc22859d870f0709",
+    "url": "/react-gh-pages/static/js/main.1bd53a95.chunk.js"
+  },
+  {
+    "revision": "f74acd4c4ca10e55731d",
+    "url": "/react-gh-pages/static/js/runtime~main.63cd1d55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
