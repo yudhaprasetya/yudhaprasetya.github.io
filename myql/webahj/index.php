5eaa62016407ecd9fa3efba9f4c60278bdef2ce5
Yudha Prasetya <yudhaprasetyausman@gmail.com>
2024-06-13 13:39:30 +0700

    add style

--- myql/webahj/index.php
+++ myql/webahj/index.php
@@ -3,10 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/style.css">
     <title>PT ANUGERAH HASEA JAYA</title>
 </head>
 <body>
-    <?php phpinfo()?>
+
+<table>
+    <thead>
+        <th>No.</th>
+        <th>Nama Pekerjaan</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>Pengadaan Materia Pembangkit Nias</td>
+        </tr>
+    </tbody>
+</table>
+    <?php 
+    ?>
     
 </body>
 </html>
