11c94047e035c128985ba3191fd656c3a2e67120
Yudha Prasetya <yudhaprasetyausman@gmail.com>
2024-11-17 04:16:58 +0700

    create php index

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cloud Hosting & Website - Yudha</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <div class="container text-center">
+        <div class="row justify-content-md-center">
+            <div class="col-md-auto p-2">
+                <h4>Cloud Hosting & Website - Yudha</h4>
+                <form action="" method="post">
+                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseLogin" aria-expanded="false" aria-controls="collapseLogin">Klik disini untuk Login</button>
+
+                    <div class="collapse" id="collapseLogin">
+                        <div class="card card-body">
+                            <input class="form-control" type="text" placeholder="login ke akun mu">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="container text-center">
+        <div class="row justify-content-md-center">
+            <div class="col-md-auto p-2">
+                <h4>Silahkan Kontak User untuk Mendaftar Website Anda</h4>
+                <form action="" method="post">
+                    <input class="form-control" type="text" placeholder="Nama Usaha">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
